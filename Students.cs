1900feaa05a88ffee74ea6a127b29746c4ec803b
Gorkem Okan <167126305+gorky77@users.noreply.github.com>
2024-09-24 20:51:33 -0400

    Add files via upload

---
+++ Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace StudentDatabaseApp.Models
+    {
+        public class Student
+        {
+            public int StudentId { get; set; }  // Students ID  (Primary Key)
+            public string FirstName { get; set; }  // Student Name
+            public string LastName { get; set; }  // Student Last Nmae 
+            public DateTime EnrollmentDate { get; set; }  // registiration date 
+        }
+    }
