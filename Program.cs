d4468a3f4654e6e2244f58da371496cc041e1b9a
Gorkem Okan <167126305+gorky77@users.noreply.github.com>
2024-09-24 20:37:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StudentDatabaseApp.Models;  // Reference to the Models namespace where Student class is located
+
+namespace StudentDatabaseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentDbContext())  // Create an instance of the database context
+            {
+                // Create a new student object
+                var student = new Student
+                {
+                    FirstName = "John",   // Assign first name
+                    LastName = "Doe",     // Assign last name
+                    EnrollmentDate = DateTime.Now  // Assign current date as enrollment date
+                };
+
+                // Add the student to the database
+                context.Students.Add(student);
+
+                // Save the changes to the database
+                context.SaveChanges();
+
+                Console.WriteLine("Student added successfully!");  // Confirmation message
+            }
+        }
+    }
+}
---
+++ StudentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using StudentDatabaseApp.Models;  
+
+namespace StudentDatabaseApp
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
+
